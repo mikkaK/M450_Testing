75f6e255c041c895cf690319639de939672f38d9
mikkakummer <mikka.kummer@noseryoung.ch>
2023-08-29 09:00:11 +0200

    removed project to work on it in original Repo

--- Projekt/Letterix_API/cover-letter-service/src/main/java/ch/letterix/coverletterservice/core/generic/AbstractRepository.java
+++
@@ -1,8 +0,0 @@
-package ch.letterix.coverletterservice.core.generic;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AbstractRepository<T extends AbstractEntity> extends JpaRepository<T, UUID> {
-}
--- Projekt/Letterix_API/cover-letter-service/src/main/java/ch/letterix/coverletterservice/domain/response/ChatCompletionRepository.java
+++
@@ -1,9 +0,0 @@
-package ch.letterix.coverletterservice.domain.response;
-
-import ch.letterix.coverletterservice.core.generic.AbstractRepository;
-import ch.letterix.coverletterservice.domain.response.entity.ChatCompletion;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatCompletionRepository extends AbstractRepository<ChatCompletion> {
-}
