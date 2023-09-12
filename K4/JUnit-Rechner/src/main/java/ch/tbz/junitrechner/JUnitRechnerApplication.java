ca270584347b75510681a9b7277ea5e46ccc3a83
mikkakummer <mikka.kummer@noseryoung.ch>
2023-09-12 09:01:39 +0200

    Finished Aufgabe 1

---
+++ K4/JUnit-Rechner/src/main/java/ch/tbz/junitrechner/JUnitRechnerApplication.java
@@ -0,0 +1,13 @@
+package ch.tbz.junitrechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnitRechnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JUnitRechnerApplication.class, args);
+    }
+
+}
